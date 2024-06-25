8755615de89f778df41612f1b0f84d1804fafef6
ShivaChandu <nedurishivachandueee@gmail.com>
2024-06-25 13:43:24 +0530

    first commit

---
+++ springboot_app_properties/src/main/java/com/app/prop/SpringbootAppPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.app.prop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppPropertiesApplication.class, args);
+	}
+
+}
---
+++ springboot_app_properties/src/test/java/com/app/prop/SpringbootAppPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.prop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
